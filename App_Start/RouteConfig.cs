63e4d45834d34fa76ca462795f1b264ace31873b
John Gwynn <jgwynn2901@yahoo.com>
2014-01-07 20:49:47 -0500

    added CRUD

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
+// ReSharper disable once CheckNamespace
 namespace HelpingHearts
 {
   public class RouteConfig
@@ -16,7 +13,8 @@ namespace HelpingHearts
       routes.MapRoute(
           name: "Default",
           url: "{controller}/{action}/{id}",
-          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+          defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+          namespaces: new[] { "HelpingHearts.Controllers" }
       );
     }
   }
