14be4583608a602788d64c5350d2a115d528da96
John Gwynn <jgwynn2901@yahoo.com>
2014-01-27 17:08:03 -0500

    Wired up edit and insert

--- Controllers/LocationController.cs
+++ Controllers/LocationController.cs
@@ -1,106 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.UI.WebControls;
+﻿using System.Web.Mvc;
+using HelpingHeartsService;
+using HelpingHeartsService.Models;
 
 namespace HelpingHearts.Controllers
 {
-    public class LocationController : Controller
+  public class LocationController : Controller
+  {
+    //
+    // GET: /Location/
+
+    public ActionResult Index()
     {
-        //
-        // GET: /Location/
+      var model = AedRepository.GetList();
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        //
-        // GET: /Location/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return Index();
-        }
-
-        //
-        // GET: /Location/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Location/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Location/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return Create();
-        }
-
-        //
-        // POST: /Location/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-              return Create();
-            }
-        }
-
-        //
-        // GET: /Location/Delete/5
-
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        ////
-        //// POST: /Location/Delete/5
-
-        //[HttpPost]
-        //public ActionResult Delete(int id, FormCollection collection)
-        //{
-        //    try
-        //    {
-        //        // TODO: Add delete logic here
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
+      return View(model);
     }
+
+    //
+    // GET: /Location/Details/5
+
+    public ActionResult Details(int id)
+    {
+      return Index();
+    }
+
+    //
+    // GET: /Location/Create
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    //
+    // POST: /Location/Create
+
+    [HttpPost]
+    public ActionResult Create(FormCollection collection)
+    {
+      try
+      {
+        // TODO: Add insert logic here
+        AedRepository.Insert(new AEdLocation(collection));
+        return RedirectToAction("Index");
+      }
+      catch
+      {
+        return View();
+      }
+    }
+
+    //
+    // GET: /Location/Edit/5
+
+    public ActionResult Edit(int id)
+    {
+      var model = AedRepository.Find(id);
+      return View(model);
+    }
+
+    //
+    // POST: /Location/Edit/5
+
+    [HttpPost]
+    public ActionResult Edit(int id, FormCollection collection)
+    {
+      try
+      {
+        // TODO: Add update logic here
+        AedRepository.Update(new AEdLocation(collection));
+        return RedirectToAction("Index");
+      }
+      catch
+      {
+        return Create();
+      }
+    }
+
+    //
+    // GET: /Location/Delete/5
+
+    //public ActionResult Delete(int id)
+    //{
+    //    return View();
+    //}
+
+    ////
+    //// POST: /Location/Delete/5
+
+    //[HttpPost]
+    //public ActionResult Delete(int id, FormCollection collection)
+    //{
+    //    try
+    //    {
+    //        // TODO: Add delete logic here
+
+    //        return RedirectToAction("Index");
+    //    }
+    //    catch
+    //    {
+    //        return View();
+    //    }
+    //}
+  }
 }
